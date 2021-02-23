e99f23500f4b2c43f858d237a7815b47dca962f7
Veritas <rey.veritas@yandex.ru>
2021-02-23 08:23:30 +0500

    Выполнил обязательное задание урока 18

---
+++ script.js
@@ -0,0 +1,37 @@
+window.addEventListener('DOMContentLoaded', function() {
+    'use strict';
+
+    // Timer
+    function countTimer(deadline) {
+        let timerHours = document.querySelector('#timer-hours'),
+            timerMinutes = document.querySelector('#timer-minutes'),
+            timerSeconds = document.querySelector('#timer-seconds'),
+            idInterval;
+
+        function getTimerRemaining() {
+            let dateStop = new Date(deadline).getTime(),
+                dateNow = new Date().getTime(),
+                timeRemaining = (dateStop - dateNow) / 1000,
+                seconds = Math.floor(timeRemaining % 60),
+                minutes = Math.floor((timeRemaining / 60) % 60),
+                hours = Math.floor(timeRemaining / 60 / 60);
+            return { timeRemaining, seconds, minutes, hours };
+        }
+
+        function updateClock() {
+            idInterval = requestAnimationFrame(updateClock);
+            let timer = getTimerRemaining();
+            timerHours.textContent = (timer.hours < 10) ? `0${timer.hours}` : timer.hours;
+            timerMinutes.textContent = (timer.minutes < 10) ? `0${timer.minutes}` : timer.minutes;
+            timerSeconds.textContent = (timer.seconds < 10) ? `0${timer.seconds}` : timer.seconds;
+
+            if (timer.timeRemaining < 0) {
+                cancelAnimationFrame(idInterval);
+                timerHours.textContent = timerMinutes.textContent = timerSeconds.textContent = '00';
+            }
+        }
+        idInterval = requestAnimationFrame(updateClock);
+    }
+
+    countTimer('8 march 2021');
+});
