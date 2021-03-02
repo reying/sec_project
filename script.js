b72294816466da89173eb6bf13a25b59e63b2cbb
Ivanov Dmitriy <rey.veritas@yandex.ru>
2021-03-02 09:59:28 +0500

    Выполнил обязательное задание урока 24

--- script.js
+++ script.js
@@ -259,13 +259,55 @@ window.addEventListener('DOMContentLoaded', function() {
 
     //калькулятор
     const calculator = () => {
-        const calc = document.getElementById('calc');
+        const calcBlock = document.querySelector('.calc-block');
 
-        calc.addEventListener('input', (event) => {
+        calcBlock.addEventListener('input', (event) => {
             if (event.target.tagName === 'INPUT') {
                 event.target.value = event.target.value.replace(/\D/, '');
             }
         });
+
+        const calc = (price = 100) => {
+            const calcType = document.querySelector('.calc-type'),
+                calcSquare = document.querySelector('.calc-square'),
+                calcDay = document.querySelector('.calc-day'),
+                calcCount = document.querySelector('.calc-count'),
+                totalValue = document.getElementById('total');
+
+            const countSum = () => {
+                let total = 0,
+                    countValue = 1,
+                    dayValue = 1;
+                const typeValue = calcType.options[calcType.selectedIndex].value,
+                    squareValue = +calcSquare.value;
+
+                if (calcCount.value > 1) {
+                    countValue += (calcCount.value - 1) / 10;
+                }
+
+                if (calcDay.value && calcDay.value < 5) {
+                    dayValue *= 2;
+                } else if (calcDay.value && calcDay.value < 10) {
+                    dayValue *= 1.5;
+                }
+
+                if (typeValue && squareValue) {
+                    total = price * typeValue * squareValue * countValue * dayValue;
+                }
+
+                totalValue.textContent = total;
+            };
+
+            calcBlock.addEventListener('change', (event) => {
+                const target = event.target;
+                if (target.matches('select') || target.matches('input')) {
+                    countSum();
+                }
+            });
+
+        };
+
+        calc(100);
     };
 
     calculator();
